822195d72ed933d4778f4c928337804c9d7f6785
Clara Chen <clara@lucidinside.com>
2023-09-08 23:16:26 -0700

    update url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -16,10 +16,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://react-video-modal.vercel.app/',
+  url: 'https://baskvava.github.io/react-video-modal/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/react-video-modal/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
