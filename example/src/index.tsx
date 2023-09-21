874b972f861e4143cdb0a14b137ab4f98218210b
clara <baskvava@gmail.com>
2023-09-20 21:12:24 -0700

    fix example dev issue

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ModalVideo, useToggle } from '../../src';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { useToggle, ModalVideo } from "../../src";
 
 function App() {
   const { isOpen, toggle, close } = useToggle();
   const {
     isOpen: isResponsiveOpen,
     toggle: responsiveToggle,
-    close: responsiveClose
+    close: responsiveClose,
   } = useToggle();
 
   return (
-    <div className="App" style={{ height: '1800px' }}>
+    <div className="App" style={{ height: "1800px" }}>
       <div>
         <span>Fixed Width Video</span>
         <button onClick={toggle}>Modal</button>
@@ -41,7 +41,7 @@ function App() {
 }
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
